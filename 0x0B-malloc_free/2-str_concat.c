ce8c7deaf575fd5fd5f9acda0df375919f0ffabe
MOKOENA9909 <mokoenalebo84@gmail.com>
2023-03-15 14:10:14 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,43 @@
+#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
 
 /**
- * *str_concat - concatenates two strings
- * @s1: string to concatenate
- * @s2: other string to concatenate
+ * str_concat - concatenates two strings
+ * @s1: string to be joined to
+ * @s2: string to join s1
  *
- * Return: pointer to the new string created (Success), or NULL (Error)
+ * Return: pointer to newly allocated space in memory
+ *         containing concatenated string , or NULL if error
+ *         in concatenating
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	char *join;
+	int copy = 0;
+	int length = 0;
+	int i;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
+	if (s1 == NULL)
+		s1 = "";
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] || s2[i]; i++)
+		length++;
+
+	join = malloc(sizeof(char) * length);
+
+	if (join == NULL)
 		return (NULL);
 
-	i = 0;
-	j = 0;
+	for (i = 0; s1[i]; i++)
+		join[copy++] = s1[i];
 
-	if (s1)
-	{
-		while (i < len1)
-		{
-			s3[i] = s1[i];
-			i++;
-		}
-	}
+	for (i = 0; s2[i]; i++)
+		join[copy++] = s2[i];
 
-	if (s2)
-	{
-		while (i < (len1 + len2))
-		{
-			s3[i] = s2[j];
-			i++;
-			j++;
-		}
-	}
-	s3[i] = '\0';
-
-	return (s3);
+	return (join);
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,44 +1,26 @@
-#include <stdlib.h>
-#include "holberton.h"
+#include &lt;stdlib.h&gt;
 
-/**
- * **alloc_grid - creates a two dimensional array of ints
- * @width: width of the matrix
- * @height: height of the matrix
- *
- * Return: pointer to the created matrix (Success)
- * or NULL (Error)
- */
-int **alloc_grid(int width, int height)
-{
-	int **array;
-	int i, j;
-
-	if (height <= 0 || width <= 0)
-		return (NULL);
-
-	array = (int **) malloc(sizeof(int *) * height);
-
-	if (array == NULL)
-		return (NULL);
-	for (i = 0; i < height; i++)
-	{
-		array[i] = (int *) malloc(sizeof(int) * width);
-		if (array[i] == NULL)
-		{
-			free(array);
-			for (j = 0; j <= i; j++)
-				free(array[j]);
-			return (NULL);
-		}
+int **alloc_grid(int width, int height) {
+	if (width &lt;= 0 || height &lt;= 0) {
+	return NULL;
 	}
 
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-		{
-			array[i][j] = 0;
-		}
+	int **grid = malloc(height * sizeof(int *));
+	if (grid == NULL) {
+	return NULL;
 	}
-	return (array);
+
+	for (int i = 0; i &lt; height; i++) {
+	grid[i] = calloc(width, sizeof(int));
+	if (grid[i] == NULL) {
+	for (int j = 0; j &lt; i; j++) {
+	free(grid[j]);
+	}
+		free(grid);
+		return NULL;
+        }
+	
+		}
+
+	return grid;
 }
