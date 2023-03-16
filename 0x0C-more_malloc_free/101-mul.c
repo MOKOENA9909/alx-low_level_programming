613bd719bdfe44d2f365c496388c3744b20306ad
MOKOENA9909 <mokoenalebo84@gmail.com>
2023-03-16 15:43:10 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,14 +1,38 @@
-#include "holberton.h"
-
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-	{
-		printf("Hel, World, World, Hel.\n");
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+**/
 
-		return (0);
+int main(int argc, char *argv[])
+{
+	unsigned long mul;
+	int i, j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
 	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
+	return (0);
+}
